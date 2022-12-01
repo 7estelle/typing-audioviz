d387d70c693a20706581e4870862dc39a970b345
7estelle <layestelle7@gmail.com>
2022-12-01 17:42:35 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,119 @@
+// PIANO
+let osc, env;
+
+let c = [0, 30, 60, 80, 150, 240, 270, 330];
+
+let keys = ['a', 'z', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'q', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'w', 'x', 'c', 'v', 'b', 'n'];
+
+let notes = [40, 42, 44, 45, 47, 49, 50, 52, 54, 55, 57, 59, 60, 62, 64, 65, 67, 69, 71, 72, 74, 76, 77, 79, 81, 83];
+
+let angle = 20;
+let offset = 200;
+let scalar = 35;
+let speedVal= 10;
+let col = {
+  r: 255,
+  g: 0,
+  b: 0
+};
+
+let rectWidth;
+let button;
+let dim;
+
+function setup() {
+  let cnv = createCanvas(400, 400);
+  cnv.parent('canvasContainer')
+  colorMode(HSB)
+  
+  env = new p5.Envelope()
+
+  env.setADSR(0.01, 0.1, 1, 0.25);
+  
+  osc = new p5.Oscillator('triangle');
+  osc.start();
+  osc.amp(env);
+  midiToFreq()
+
+  rectWidth = width/4;
+
+  dim = width / 2;
+  background(0);
+  colorMode(HSB, 360, 100, 100);
+  noStroke();
+  ellipseMode(RADIUS);
+  frameRate(1);
+
+//   SAVE BUTTON
+  button = createButton('Save Canvas');
+  button.mousePressed(savePalette);
+}
+
+function savePalette() {
+    saveCanvas('myCanvas', 'png');
+}
+
+
+
+function draw() {
+  // background(220, 49, 150);
+
+  // for(let i = 0; i < notes.length; i++) {
+  //   // fill(0, 0, 100)
+  //   if(keyIsPressed && key == keys[i]){
+  //       // rect(cos(200 + i), sin(200 + i), i * 4, i*4)
+  //       // fill(random(1, 255), 100, 100)
+  //       // noStroke()
+
+  //       col.r = random(0, 200);
+  //       col.g = random(0, 250);
+  //       col.b = random(100, 250);
+  //       let x = offset + cos(angle) * scalar;
+  //       let y = offset + sin(angle) * scalar;
+  //       fill(col.r, col.g, col.b);
+  //       noStroke();
+  //       ellipse(x, y, 10, 10);
+  //       angle += speed;
+  //       scalar += speed;
+  //       // osc.freq(midiToFreq(notes[i])); 
+  //   }
+  // }
+
+}
+
+
+function drawGradient() {
+  let radius = dim / 2;
+  let h = random(0, 360);
+  let x = offset + cos(angle) * scalar;
+  let y = offset + sin(angle) * scalar;
+  for (let r = radius; r > 0; --r) {
+    fill(h, 90, 90);
+    ellipse(x, y, r, r);
+    h = (h + 1) % 360;
+  }
+  angle += speedVal;
+  scalar += speedVal;
+}
+
+
+function keyPressed() {
+    let keyIndex = -1;
+    if (key >= 'a' && key <= 'z') {
+      keyIndex = key.charCodeAt(0) - 'a'.charCodeAt(0);
+      osc.freq(midiToFreq(notes[keyIndex])); 
+    }
+    if (keyIndex === -1) {
+      // If it's not a letter key, clear the screen
+      background(230);
+      angle = 20;
+      offset = 200;
+      scalar = 35;
+      speedVal = 10;
+    } else {
+        for (let x = 0; x <= width; x += dim) {
+          drawGradient(x, height / 2);
+        }
+    }
+  env.play()
+}
